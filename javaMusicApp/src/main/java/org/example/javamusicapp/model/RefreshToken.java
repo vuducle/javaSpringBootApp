a08d4bdbe95b095dbc5bdfd9e1de6d96de2294ec
Minh Le <vuminhle@outlook.com>
2025-11-13 16:50:19 +0700

    adding refreshToken for model and repo

---
+++ javaMusicApp/src/main/java/org/example/javamusicapp/model/RefreshToken.java
@@ -0,0 +1,31 @@
+package org.example.javamusicapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name="refresh_token")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Date expiryDate;
+}
---
+++ javaMusicApp/src/main/java/org/example/javamusicapp/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package org.example.javamusicapp.repository;
+
+import org.example.javamusicapp.model.RefreshToken;
+import org.example.javamusicapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    // Suche den Token-Eintrag anhand des langen Token-Strings
+    Optional<RefreshToken> findByToken(String token);
+
+    // Lösche den Token für diesen Benutzer, z.B. bei Logout
+    void deleteByUser(User user);
+}
